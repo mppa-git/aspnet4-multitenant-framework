8f355c68c5348d8c03eee9d6a2d95b57b3269047
Scott Stafford <stafford_scott_e@bah.com>
2015-11-24 15:12:31 -0500

    Added SubdomainRoute.cs from http://stackoverflow.com/a/15287579/237091

--- Shell/App_Start/RouteConfig.cs
+++ Shell/App_Start/RouteConfig.cs
@@ -4,16 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Bah.Core.Site.Multitenancy;
 
 namespace Shell
 {
+
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapSubdomainRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
